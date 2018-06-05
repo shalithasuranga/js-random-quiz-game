d08a3a319487f465eebf367fbd05d712c2a92c74
Shalitha Suranga <shalithasuranga@gmail.com>
2018-06-05 13:34:22 +0530

    Fix game funcs

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 let level = 0;
-let gcount = 10;
+let gcount = 300;
 let status = 0;
-console.log(gcount);
 
 
 
@@ -13,12 +12,12 @@ let getRange = () => {
         min = 1;
     }
     else if(level == 1){
-        max = 10;
+        max = 15;
         min = 5;
     }
     else if(level == 2){
-        max = 15;
-        min = 10;
+        max = 30;
+        min = 15;
     }
     return {
         max : max,
@@ -29,33 +28,34 @@ let getRange = () => {
 let getRand = () => {
     let max = getRange().max;
     let min = getRange().min;
-    let rand =  Math.floor(Math.random() * max) + min ;
+    let rand =  Math.floor(Math.random()*(max-min+1)+min);
     console.log(rand);
     return rand;
 }
 
 let play = () => {
-    let end = getRange().max;
-    let start = getRange().min;
+    let end = 30;
+    let start = 1;
     console.log(getRange());
     let i = start;
     let an = setInterval(() => {
-        document.getElementById('number').innerHTML = i + 1;
+        document.getElementById('number').innerHTML = (i + 1).toString().padStart(2,"0");
         i++;
         if(i == end) {
             i = start;
         }
         gcount --;
         if(gcount == 0) {
-            document.getElementById('number').innerHTML = getRand();
+            document.getElementById('number').innerHTML = getRand().toString().padStart(2,"0");
             document.getElementById('action').style.display = '';
             document.getElementById('action').innerHTML = 'Question';
+            document.getElementById('action').classList.add('actiond');
             clearInterval(an);
-            gcount = 10;
+            gcount = 300;
             status = 1;
             level ++;
         }
-    }, 100);
+    }, 20);
 };
 
 let startGame = () => {
@@ -97,16 +97,18 @@ window.onload = () => {
     document.getElementById('question').style.display = 'none';
     document.getElementById('counter').style.display = '';
     document.getElementById('number').style.display = 'none';
+    document.getElementById('thank').style.display = 'none';
 };
 
 
 let backtogame = () => {
     if(level < 3) {
+        document.getElementById('action').classList.remove('actiond');
         document.getElementById('counter').style.display = '';
         document.getElementById('question').style.display = 'none';
         document.getElementById('number').style.display = 'none';
     }
     else {
-        location.reload();
+        document.getElementById('thank').style.display = '';
     }
 };
